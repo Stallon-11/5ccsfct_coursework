c96af18c7fe5f25379b0ebd475ff9174df5aa866
Stallon Fernandes <stallon.fernandes@outlook.com>
2025-11-18 22:30:23 +0000

    q1 (iii)

--- questionA.py
+++ questionA.py
@@ -1,5 +1,6 @@
 from automata.fa.dfa import DFA
 from automata.fa.nfa import NFA
+import re
 
 A1 = DFA(states = {'q0','q1','q2','q3','q4','q5','q6'},
 		  initial_state='q0',
@@ -29,12 +30,23 @@ A2 = DFA(states = {'q0','q1','q2','q3','q4','q5'},
 			  'q5': {'a':'q5', 'b':'q5', 'c':'q5'},
           })
 
-# A3 = NFA(states = {},
-# 		  initial_state=,
-# 		  final_states = {},
-# 		  input_symbols={'a','b', 'c'},
-# 		  transitions = {
-#           })
+A3 = NFA(states = {'q0','q1','q2','q3','q4','q5','q6','q7','q8','q9','q10'},
+		  initial_state='q0',
+		  final_states = {'q10'},
+		  input_symbols={'a','b', 'c'},
+		  transitions = {
+			  'q0': {'a': {'q0'}, 'b': {'q0', 'q1'}, 'c': {'q0'}},
+			  'q1': {'a': {'q2'}, 'b': {'q1'}, 'c': {'q0'}},
+			  'q2': {'a': {'q0'}, 'b': {'q3'}, 'c': {'q0'}},
+			  'q3': {'a': {'q2'}, 'b': {'q1'}, 'c': {'q4'}},
+			  'q4': {'a': {'q0'}, 'b': {'q5'}, 'c': {'q0'}},
+			  'q5': {'a': {'q5'}, 'b': {'q5', 'q6'}, 'c': {'q5'}},
+			  'q6': {'a': {'q7'}, 'b': {'q6'}, 'c': {'q5'}},
+			  'q7': {'a': {'q5'}, 'b': {'q8'}, 'c': {'q5'}},
+			  'q8': {'a': {'q7'}, 'b': {'q6'}, 'c': {'q9'}},
+			  'q9': {'a': {'q5'}, 'b': {'q10'}, 'c': {'q5'}},
+			  'q10': {'a': {'q10'}, 'b': {'q10'}, 'c': {'q10'}}
+          })
 #
 # A4 = NFA(states = {},
 # 		  initial_state=,
@@ -42,6 +54,7 @@ A2 = DFA(states = {'q0','q1','q2','q3','q4','q5'},
 # 		  input_symbols={'a','b', 'c'},
 # 		  transitions = {
 #           })
+
 
 
 if __name__ == "__main__":
@@ -62,46 +75,28 @@ if __name__ == "__main__":
 							for s6 in alphabet:
 								for s7 in alphabet:
 									for s8 in alphabet:
-										test.append(f"{s0}{s1}{s2}{s3}{s4}{s5}{s6}{s7}{s8}")
+										for s9 in alphabet:
+											for s10 in alphabet:
+												for s11 in alphabet:
+													for s12 in alphabet:
+														test.append(
+															f"{s0}{s1}{s2}{s3}{s4}{s5}{s6}{s7}{s8}{s9}{s10}{s11}{s12}")
+														# for s13 in alphabet:
+														# 	for s14 in alphabet:
+														#
+														# 		test.append(f"{s0}{s1}{s2}{s3}{s4}{s5}{s6}{s7}{s8}{s9}{s10}{s11}{s12}{s13}{s14}")
 
 
 	for word in test:
-		if A1.accepts_input(word):
+		if A3.accepts_input(word):
 			#print(f"{s!r} → ACCEPTED")
 			accepted.append(word)
 		else:
 			rejected.append(word)
 			#print(f"{s!r} → REJECTED")
 
+
+
 	# print(f"Accepted: {accepted}\n")
 	# print(f"Rejected: {rejected}\n")
 
- 	# Test for q1 (i)
-	# correct = 0
-	# for word in accepted:
-	# 	if string == word[0:len(string)]:
-	# 		correct += 1
-	# print(f"{(correct / len(accepted)) * 100}% of the accepted list, contains {string}")
-	#
-	# correct = 0
-	# for word in rejected:
-	# 	if string == word[0:len(string)]:
-	# 		correct += 1
-	# print(f"{(correct / len(rejected)) * 100}% of the rejected list, contains {string}")
-
-
-	# Test for Q2 (ii)
-	# correct = 0
-	# for word in accepted:
-	# 	if string in word:
-	# 		correct += 1
-	#
-	# print(f"{(correct / len(accepted) ) * 100}% of the accepted list, contains {string}")
-	#
-	# correct = 0
-	# for word in rejected:
-	# 	if string in word:
-	# 		print("Rejected: "+word)
-	# 		correct += 1
-	#
-	# print(f"{(correct / len(rejected)) * 100}% of the rejected list, contains {string}")
